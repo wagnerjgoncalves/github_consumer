31f8be74c5dcfe02392e4de74fc82b211fcbf7f2
Wagner J Goncalves <wagner@tuneduc.com.br>
2018-02-10 13:14:24 -0200

    refactor: stats contributors

--- lib/github_consumer.rb
+++ lib/github_consumer.rb
@@ -1,19 +1,57 @@
 require 'dotenv/load'
-require 'github_consumer/version'
+require 'github_consumer/exporter'
 require 'github_consumer/http'
+require 'github_consumer/version'
 
 module GithubConsumer
+  DEFAULT_FILE_HEADERS = %w(name email login avatar_url commits_count).freeze
+
   module_function
 
-  # Fetch commits from a specific repository
+  # Fetch commits from a specific repository of a owner and then,
+  # export a txt file status report with all commiters ordered by total commits
   #
   # Example:
-  #   >> GithubConsumer.fetch_commits(owner, repository)
+  #   >> GithubConsumer.stats_contributors(owner, repository)
   #
   # Arguments:
   #   owner: (String)
   #   repository: (String)
-  def fetch_commits(owner, repository)
-    Http.new(owner, repository).fetch_commits
+  def stats_contributors(owner, repository)
+    response = Http.stats_contributors(owner, repository)
+
+    return response if response[:status] == :error
+
+    body = []
+    file_name = "#{owner}_#{repository}"
+
+    data = response[:data].sort_by { |item| item[:total] }.reverse
+    data.each do |status_data|
+      details = fetch_user_details(status_data[:author][:login])
+
+      body << details + [status_data[:total]] unless details.empty?
+    end
+
+    Exporter.save(GithubConsumer::DEFAULT_FILE_HEADERS, body, file_name)
+  end
+
+  # Fetch user details by login
+  #
+  # Example:
+  #   >> GithubConsumer.fetch_user_details(login)
+  #
+  # Arguments:
+  #   login: (String)
+  def fetch_user_details(login)
+    details = Http.user_details(login)
+
+    return [] if details[:status] == :error
+
+    [
+      details[:data][:name],
+      details[:data][:email],
+      details[:data][:login],
+      details[:data][:avatar_url]
+    ]
   end
 end
--- spec/github_consumer_spec.rb
+++ spec/github_consumer_spec.rb
@@ -1,28 +1,50 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe GithubConsumer do
   it 'has a version number' do
     expect(GithubConsumer::VERSION).not_to be nil
   end
 
-  describe '#fetch_commits' do
+  describe '#stats_contributors' do
     let(:owner) { 'git' }
     let(:repository) { 'sample' }
-
-     before do
-      allow_any_instance_of(GithubConsumer::Http).to receive(:get).and_return(mock)
+    let(:stats_mock) do
+      {
+        status: :ok,
+        data: [{
+          author: { login: owner },
+          total: 10
+        }]
+      }
     end
 
-    describe 'with a sucessfuly request' do
-      let(:mock) { { status: :ok } }
+    before do
+      allow(GithubConsumer::Http).to receive(:user_details).and_return(user_details_mock)
+      allow(GithubConsumer::Http).to receive(:stats_contributors).and_return(stats_mock)
+    end
 
-      it { expect(described_class.fetch_commits(owner, repository)[:status]).to eq :ok }
+    subject { GithubConsumer.stats_contributors(owner, repository) }
+
+    describe 'with a sucessfuly request' do
+      let(:user_details_mock) do
+        {
+          status: :ok,
+          data: {
+            name: 'Jhon Doe',
+            email: 'jhondoe@example.com',
+            login: 'jhondoe',
+            avatar_url: 'http://jhondoe.jpg'
+          }
+        }
+      end
+
+      it { expect(subject[:status]).to eq :ok }
     end
 
     describe 'with an unsucessfuly request' do
-      let(:mock) { { status: :error } }
+      let(:user_details_mock) { { status: :error, message: 'Not Found' } }
 
-      it { expect(described_class.fetch_commits(owner, repository)[:status]).to eq :error }
+      it { expect(subject[:status]).to eq :error }
     end
   end
 end
