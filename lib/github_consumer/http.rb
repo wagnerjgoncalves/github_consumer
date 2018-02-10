17709dd6a2e57b786fa8370d241ab43f1ed4c766
Wagner J Goncalves <wagner@tuneduc.com.br>
2018-02-10 13:13:10 -0200

    refactor: http

--- lib/github_consumer/http.rb
+++ lib/github_consumer/http.rb
@@ -2,37 +2,59 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class GithubConsumer::Http
-  HOST = ENV['HOST']
-  TOKEN = ENV['TOKEN']
+module GithubConsumer
+  class Http
+    HOST = ENV['HOST']
+    TOKEN = ENV['TOKEN']
 
-  attr_reader :owner, :repository
+    # Fetch GitHub contributos status
+    #
+    # Example:
+    #   >> GithubConsumer::Http.stats_contributors(owner, repository)
+    #
+    # Arguments:
+    #   owner: (String)
+    #   repository: (String)
+    def self.stats_contributors(owner, repository)
+      url = "#{HOST}/repos/#{owner}/#{repository}/stats/contributors"
 
-  def initialize(owner, repository)
-    @owner = owner
-    @repository = repository
-  end
-
-  def fetch_commits
-    url = "#{HOST}/repos/#{owner}/#{repository}/commits"
-
-    get url
-  end
-
-  private
-
-  def get(url)
-    uri = URI(url)
-
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.request Net::HTTP::Get.new uri, { 'Authorization' => "token #{TOKEN}" }
+      get url
     end
 
-    case response
-    when Net::HTTPSuccess, Net::HTTPOK
-      { status: :ok, data: JSON.parse(response.body) }
-    else
-      { status: :error, data: response.message }
+    # Fetch GitHub user details
+    #
+    # Example:
+    #   >> GithubConsumer::Http.user_details(login)
+    #
+    # Arguments:
+    #   login: (String)
+    def self.user_details(login)
+      url = "#{HOST}/users/#{login}"
+
+      get url
+    end
+
+    # Execute GET requests at a GitHub using TOKEN Authorization
+    #
+    # Example:
+    #   >> GithubConsumer::Http.get(url)
+    #
+    # Arguments:
+    #   url: (String)
+    def self.get(url)
+      authorization = { 'Authorization' => "token #{TOKEN}" }
+      uri = URI(url)
+
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        http.request Net::HTTP::Get.new uri, authorization
+      end
+
+      case response
+      when Net::HTTPSuccess, Net::HTTPOK
+        { status: :ok, data: JSON.parse(response.body, symbolize_names: true) }
+      else
+        { status: :error, message: response.message }
+      end
     end
   end
 end
